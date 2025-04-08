2389bdd6bf34c0283cb4cbdd129b4ad2e2d05132
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-04-08 12:10:24 +0530

    Add link to AI Features in Hero component

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -197,6 +197,7 @@ export default function HeroGeometric({
                   </p>
                 </Magnet>
               </Link>
+              <a href="https://67f48122b61e4c4cc9e6aa22--ai-image-editor-cap.netlify.app/" target="_blank" className=" font-bold border-2 rounded-2xl p-4 border-amber-50 whitespace-nowrap text-sm hover:text-bolt-elements-textPrimary transition-theme relative mr-1">AI Features<span className=" absolute -right-2 -top-0.5"><svg xmlns="http://www.w3.org/2000/svg" width="5" height="6" viewBox="0 0 10 12" fill="none" className="animate-pulse animate-delay-500 absolute -left-[3px] -top-[3px]"><path fill="#1488FC" d="M5.332 1.064a.5.5 0 0 0-.972 0c-.37 1.528-.76 2.45-1.315 3.092-.547.635-1.298 1.05-2.513 1.461a.5.5 0 0 0 0 .948c1.27.428 2.03.88 2.568 1.528.546.657.908 1.57 1.26 3.024a.5.5 0 0 0 .972 0c.37-1.527.761-2.448 1.314-3.091.547-.635 1.298-1.05 2.514-1.461a.5.5 0 0 0 0-.948c-1.272-.43-2.032-.88-2.568-1.526-.546-.656-.907-1.568-1.26-3.027Z"></path></svg><svg xmlns="http://www.w3.org/2000/svg" width="8" height="9.6" viewBox="0 0 10 12" fill="none" class="animate-pulse"><path fill="#1488FC" d="M5.332 1.064a.5.5 0 0 0-.972 0c-.37 1.528-.76 2.45-1.315 3.092-.547.635-1.298 1.05-2.513 1.461a.5.5 0 0 0 0 .948c1.27.428 2.03.88 2.568 1.528.546.657.908 1.57 1.26 3.024a.5.5 0 0 0 .972 0c.37-1.527.761-2.448 1.314-3.091.547-.635 1.298-1.05 2.514-1.461a.5.5 0 0 0 0-.948c-1.272-.43-2.032-.88-2.568-1.526-.546-.656-.907-1.568-1.26-3.027Z"></path></svg></span></a>
             </div>
           </motion.div>
         </div>
